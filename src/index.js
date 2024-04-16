ca85b5050d4819cfec28fabbee1deb6e2d0e0514
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-04-17 04:50:08 +0530

    index and list attestations

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Route exact path="/" component={Home} />
-      <Route path="/token-list" component={List} />
+      <Route path="/schemas" component={List} />
       <Route path="/why" component={Why} />
     </Router>
   </React.StrictMode>,
