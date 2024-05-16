100255f30315076317dfe0ac3301b6e4c7ebd27d
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-05-16 23:41:28 +0530

    add arbi1

--- src/pages/prettyform.js
+++ src/pages/prettyform.js
@@ -184,6 +184,7 @@ export default function PrettyForm() {
                                                 })} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-black-600 sm:max-w-xs sm:text-sm sm:leading-6">
                                                     <option disabled value="1">Ethereum</option>
                                                     <option value="10">Optimism</option>
+                                                    <option value="42161">Arbitrum-One</option>
                                                     <option disabled value="137">Polygon</option>
                                                 </select>
                                             </div>
